3af9c7f331726204d75189db374840784efa3080
Christopher Purnell <112015234+Ctpurnell@users.noreply.github.com>
2023-02-08 19:46:28 -0500

    started routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const imageRoutes = require('./imageRoutes');
+const userRoutes = require('./userRoutes');
 
 router.use('/images', imageRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,4 +2,21 @@ const router = require('express').Router();
 const { Image, User } = require('../models');
 const withAuth = require('../utils/auth');
 
+router.get('/', async (req,res) => {
+    try{
+        const imageData = await Image.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+
+        const images = imageData.map((image) => image.get({ plain: true}));
+
+        res.render('')
+    }
+})
+
 module.exports = router;
